1b66b247658052cf1375b0b56d5368c2c630e5ac
Michael Bentley <c15michael.bentley@usafa.edu>
2013-11-07 13:17:17 -0700

    First compiling version that can actually display things on the LCD.  Buttons are starting to work.  Buttons use iterrupts that set global variable now and minimize work in the ISR.

--- main.c
+++ main.c
@@ -1,14 +1,11 @@
 #include <msp430.h> 
 #include "game_shell/game.h"
 #include "LCD/LCD.h"
-
-#define DIR_UP 1
-#define DIR_DOWN 2
-#define DIR_LEFT 3
-#define DIR_RIGHT 4
+#include "buttons/button.h"
 
 unsigned char direction;
-unsigned char moveDetected;
+unsigned char moveDetected = 0;
+char player = 0;
 
 /*
  * main.c
@@ -22,25 +19,28 @@ int main(void) {
 	init_timer();
 	init_buttons();
 	__enable_interrupt();
+	initSPI();
+	LCDinit();
 
 	while (1) {
-		/*
-		 * while (game is on)
-		 * {
-		 * 		check if button is pushed (you don't want to block here, so don't poll!)
-		 * 		if button is pushed:
-		 * 			clear current player marker
-		 * 			update player position based on direction
-		 * 			print new player
-		 * 			clear two second timer
-		 * 			wait for button release (you can poll here)
-		 * }
-		 *
-		 * print game over or you won, depending on game result
-		 *
-		 * wait for button press to begin new game (you can poll here)
-		 * wait for release before starting again
-		 */
+
+		LCDclear();
+		player = 0x80;
+		printPlayer(player);
+
+		char gameOver = 0;
+
+		while (gameOver == 0) {
+
+			if (moveDetected != 0) {
+				player = movePlayer(player, moveDetected);
+				moveDetected = 0;
+			}
+
+			if(player == 0x87) {
+				gameOver = 1;
+			}
+		}
 
 	}
 
@@ -57,35 +57,33 @@ void init_timer() {
 
 void init_buttons() {
 
-	P1DIR &= ~(BIT1 | BIT2 | BIT3 | BIT4);               // set buttons to input
+	configureP1PinAsButton(BIT1 | BIT2 | BIT3 | BIT4);   // set buttons to input
 
-	P1IE |= BIT1 | BIT2 | BIT3 | BIT4;                 // enable the interrupts
+	P1IE |= BIT1 | BIT2 | BIT3 | BIT4;                  // enable the interrupts
 	P1IES |= BIT1 | BIT2 | BIT3 | BIT4; // configure interrupt to sense falling edges
-
-	P1REN |= BIT1 | BIT2 | BIT3 | BIT4; // enable internal pull-up/pull-down network
-	P1OUT |= BIT1 | BIT2 | BIT3 | BIT4;                  // configure as pull-up
 
 	P1IFG &= ~(BIT1 | BIT2 | BIT3 | BIT4);                // clear flags
 }
 
 #pragma vector=PORT1_VECTOR
 __interrupt void Port_1_ISR(void) {
+
 	if (P1IFG & BIT1) {
-		P1IFG &= ~BIT1;                           // clear flag
-		direction = DIR_UP;
+		P1IFG &= ~BIT1;                         // clear flag
+		moveDetected = UP;
 	}
 
 	if (P1IFG & BIT2) {
 		P1IFG &= ~BIT2;                         // clear flag
-		direction = DIR_DOWN;
+		moveDetected = DOWN;
 	}
 
 	if (P1IFG & BIT3) {
-		P1IFG &= ~BIT3;                         // clear P1.3 interrupt flag
-		direction = DIR_LEFT;
+		P1IFG &= ~BIT3;                         // clear flag
+		moveDetected = LEFT;
 	}
 	if (P1IFG & BIT4) {
 		P1IFG &= ~BIT4;
-		direction = DIR_RIGHT;
+		moveDetected = RIGHT;
 	}
 }
